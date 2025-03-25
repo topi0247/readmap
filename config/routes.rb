5c88f5dae771305a0e3a7076d774a9b6fbdbb772
sumisumi2000 <sumio.vantan308@gmail.com>
2025-03-25 21:51:40 +0900

    feat: セッションコントローラとビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root to: "static_pages#index"
+
+  get "/login", to: "sessions#new", as: "login"
 end
