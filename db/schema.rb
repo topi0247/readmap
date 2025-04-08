8404561ffa2ee4cbe7d1ec281e8118cf9d499e13
sumisumi2000 <sumio.vantan308@gmail.com>
2025-04-08 22:25:18 +0900

    feat: categories テーブルに制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_08_131459) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_08_132412) do
   create_table "authentications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "email", null: false
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_08_131459) do
   end
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
