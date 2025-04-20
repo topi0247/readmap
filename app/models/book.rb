d04b7482621c50fc1a9910d0ad49aebb374f23f5
topi0247 <karubona.ra247@gmail.com>
2025-04-20 21:26:52 +0900

    feat: バリデーション追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,9 @@ class Book < ApplicationRecord
   has_many :categories, through: :book_categories
   has_many :list_books, dependent: :destroy
   has_many :lists, through: :list_books
-  
+
   validates :title, presence: true
+  validates :url, presence: true, format: { with: URI::regexp(%w[http https]), message: 'URLが不正です' }
+  validates :isbn, presence: true, uniqueness: true, format: { with: /\A\d{10}(\d{3})?\z/, message: 'ISBNは10桁または13桁の数字でなければなりません' }
+  validates :cover_image_url, presence: true, format: { with: URI::regexp(%w[http https]), message: '表紙画像のURLが不正です' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :lists, dependent: :destroy
   has_one :authentication, dependent: :destroy
-  
+
   validates :name, presence: true
 end
