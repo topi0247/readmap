7749d0bdc5124ebc1a979d1447b80428c0be544b
sumisumi2000 <sumio.vantan308@gmail.com>
2025-04-30 22:16:10 +0900

    feat: ユーザーが非公開になるとリストを全て非公開にする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
+      # ユーザーが非公開になるとリストも全て非公開にする
+      unless @user.is_public
+        @user.lists.each do |list|
+          list.is_public = false
+          list.save!
+        end
+      end
       redirect_to user_path(params[:id])
     else
       render :edit, status: :unprocessable_entity
