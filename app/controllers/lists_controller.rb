a6220c155b70b1e105a9a1530cf3337364423bdb
topi0247 <karubona.ra247@gmail.com>
2025-05-03 18:50:01 +0900

    feat: リスト作成と編集と削除実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,23 @@
 class ListsController < ApplicationController
+  before_action :set_current_user_list, only: [:edit, :update, :destroy]
+
   def index
     # TODO: ログインユーザーも除く
     @non_current_users = User.where(is_public: true).includes(:lists)
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = current_user.lists.build(list_params)
+    if @list.save
+      redirect_to new_list_book_path(list_id: @list.id), success: 'リストを作成しました'
+    else
+      flash.now[:warning] = 'リスト作成に失敗しました'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -15,9 +25,36 @@ class ListsController < ApplicationController
     @user = User.find_by(id: 1)
   end
 
+  def edit; end
+
   def update
+    if @list.update(list_params)
+      redirect_to list_path(@list), success: 'リストを更新しました'
+    else
+      flash.now[:warning] = 'リスト更新に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    if @list.destroy
+      redirect_to user_path(current_user), success: 'リストを削除しました'
+    else
+      flash.now[:warning] = 'リスト削除に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :is_public)
+  end
+
+  def set_current_user_list
+    @list = current_user.lists.find_by(id: params[:id])
+    unless @list
+      redirect_to lists_path, error: "リストが見つかりません"
+    end
   end
 end
